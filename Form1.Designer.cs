5a467f147ed9cd3c5d5d0ed81c78577d9b87ba14
K4miru <k.g.tracz@gmail.com>
2018-05-17 01:02:55 +0200

    Working lan comunicator between computers C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lAddress = new System.Windows.Forms.Label();
+            this.lPort = new System.Windows.Forms.Label();
+            this.lbSerwer = new System.Windows.Forms.ListBox();
+            this.bStart = new System.Windows.Forms.Button();
+            this.bStop = new System.Windows.Forms.Button();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.wB1 = new System.Windows.Forms.WebBrowser();
+            this.cMSWeb = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tB1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.tBNick = new System.Windows.Forms.TextBox();
+            this.Nick = new System.Windows.Forms.Label();
+            this.btBold = new System.Windows.Forms.Button();
+            this.btItalic = new System.Windows.Forms.Button();
+            this.cBAddress = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lAddress
+            // 
+            this.lAddress.AutoSize = true;
+            this.lAddress.Location = new System.Drawing.Point(9, 69);
+            this.lAddress.Name = "lAddress";
+            this.lAddress.Size = new System.Drawing.Size(45, 13);
+            this.lAddress.TabIndex = 0;
+            this.lAddress.Text = "Address";
+            // 
+            // lPort
+            // 
+            this.lPort.AutoSize = true;
+            this.lPort.Location = new System.Drawing.Point(204, 69);
+            this.lPort.Name = "lPort";
+            this.lPort.Size = new System.Drawing.Size(26, 13);
+            this.lPort.TabIndex = 1;
+            this.lPort.Text = "Port";
+            // 
+            // lbSerwer
+            // 
+            this.lbSerwer.FormattingEnabled = true;
+            this.lbSerwer.Location = new System.Drawing.Point(12, 12);
+            this.lbSerwer.Name = "lbSerwer";
+            this.lbSerwer.Size = new System.Drawing.Size(510, 43);
+            this.lbSerwer.TabIndex = 2;
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(382, 64);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(140, 22);
+            this.bStart.TabIndex = 3;
+            this.bStart.Text = "Start";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // bStop
+            // 
+            this.bStop.Enabled = false;
+            this.bStop.Location = new System.Drawing.Point(383, 498);
+            this.bStop.Name = "bStop";
+            this.bStop.Size = new System.Drawing.Size(140, 23);
+            this.bStop.TabIndex = 4;
+            this.bStop.Text = "Wyślij";
+            this.bStop.UseVisualStyleBackColor = true;
+            this.bStop.Click += new System.EventHandler(this.bStop_Click);
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(237, 67);
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(140, 20);
+            this.numPort.TabIndex = 5;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // wB1
+            // 
+            this.wB1.ContextMenuStrip = this.cMSWeb;
+            this.wB1.Location = new System.Drawing.Point(11, 124);
+            this.wB1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wB1.Name = "wB1";
+            this.wB1.Size = new System.Drawing.Size(511, 335);
+            this.wB1.TabIndex = 7;
+            // 
+            // cMSWeb
+            // 
+            this.cMSWeb.Name = "cMSWeb";
+            this.cMSWeb.Size = new System.Drawing.Size(61, 4);
+            // 
+            // tB1
+            // 
+            this.tB1.Location = new System.Drawing.Point(12, 500);
+            this.tB1.Name = "tB1";
+            this.tB1.Size = new System.Drawing.Size(365, 20);
+            this.tB1.TabIndex = 8;
+            this.tB1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tB1_KeyDown);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerSupportsCancellation = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // tBNick
+            // 
+            this.tBNick.Location = new System.Drawing.Point(60, 98);
+            this.tBNick.Name = "tBNick";
+            this.tBNick.Size = new System.Drawing.Size(317, 20);
+            this.tBNick.TabIndex = 9;
+            // 
+            // Nick
+            // 
+            this.Nick.AutoSize = true;
+            this.Nick.Location = new System.Drawing.Point(25, 101);
+            this.Nick.Name = "Nick";
+            this.Nick.Size = new System.Drawing.Size(29, 13);
+            this.Nick.TabIndex = 10;
+            this.Nick.Text = "Nick";
+            // 
+            // btBold
+            // 
+            this.btBold.Location = new System.Drawing.Point(11, 465);
+            this.btBold.Name = "btBold";
+            this.btBold.Size = new System.Drawing.Size(26, 23);
+            this.btBold.TabIndex = 11;
+            this.btBold.Text = "B";
+            this.btBold.UseVisualStyleBackColor = true;
+            this.btBold.Click += new System.EventHandler(this.btBold_Click);
+            // 
+            // btItalic
+            // 
+            this.btItalic.Location = new System.Drawing.Point(43, 465);
+            this.btItalic.Name = "btItalic";
+            this.btItalic.Size = new System.Drawing.Size(26, 23);
+            this.btItalic.TabIndex = 12;
+            this.btItalic.Text = "/";
+            this.btItalic.UseVisualStyleBackColor = true;
+            this.btItalic.Click += new System.EventHandler(this.btItalic_Click);
+            // 
+            // cBAddress
+            // 
+            this.cBAddress.FormattingEnabled = true;
+            this.cBAddress.Location = new System.Drawing.Point(60, 66);
+            this.cBAddress.Name = "cBAddress";
+            this.cBAddress.Size = new System.Drawing.Size(140, 21);
+            this.cBAddress.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 562);
+            this.Controls.Add(this.cBAddress);
+            this.Controls.Add(this.btItalic);
+            this.Controls.Add(this.btBold);
+            this.Controls.Add(this.Nick);
+            this.Controls.Add(this.tBNick);
+            this.Controls.Add(this.tB1);
+            this.Controls.Add(this.wB1);
+            this.Controls.Add(this.numPort);
+            this.Controls.Add(this.bStop);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.lbSerwer);
+            this.Controls.Add(this.lPort);
+            this.Controls.Add(this.lAddress);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lAddress;
+        private System.Windows.Forms.Label lPort;
+        private System.Windows.Forms.ListBox lbSerwer;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Button bStop;
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.WebBrowser wB1;
+        private System.Windows.Forms.TextBox tB1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.TextBox tBNick;
+        private System.Windows.Forms.Label Nick;
+        private System.Windows.Forms.Button btBold;
+        private System.Windows.Forms.Button btItalic;
+        private System.Windows.Forms.ComboBox cBAddress;
+        private System.Windows.Forms.ContextMenuStrip cMSWeb;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        IPGlobalProperties propertiesIP = IPGlobalProperties.GetIPGlobalProperties();
+        private TcpListener server = null;
+        private TcpClient client =null;
+        private BinaryReader reading = null;
+        private BinaryWriter writing = null;
+        private bool activeCall = false;
+        private bool bold = false;
+        private bool italic= false;
+        [DllImport("user32.dll")]
+        static extern bool FlashWindow(IntPtr hwnd, bool bInvert);
+        public void Combo()
+        {
+            string priev = "";
+            foreach (IPEndPoint connectUdp in propertiesIP.GetActiveUdpListeners())
+            {
+                char[] chartab = connectUdp.Address.ToString().ToCharArray();
+                if (chartab[0].ToString() != "0" && chartab[0].ToString() != ":" && chartab[0].ToString() != "f")
+                {
+                    
+                    if (priev != connectUdp.Address.ToString())
+                    {
+                        cBAddress.Items.Add(connectUdp.Address.ToString());
+                        priev = connectUdp.Address.ToString();
+                    }
+                }
+                    
+            }
+        }
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            Combo();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (!activeCall)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else
+            {
+                activeCall = false;
+                backgroundWorker1.CancelAsync();
+                writing.Write("END");
+                client.Close();
+                server.Stop();
+                lbSerwer.Items.Add("Zakończono pracę serwera ...");
+                bStart.Enabled = true;
+                bStart.Text = "Start";
+                tBNick.Enabled = true;
+                bStop.Enabled = false;
+            }
+        }
+
+        
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            if (lbSerwer.InvokeRequired || cBAddress.InvokeRequired)
+            {
+                IPAddress adresIP = null;
+                string host = "";
+                cBAddress.Invoke(new MethodInvoker(delegate { 
+                    if(cBAddress.SelectedItem!=null)
+                        host = cBAddress.SelectedItem.ToString();
+                    else
+                        host = cBAddress.Text.ToString();
+
+                }));
+                int port = System.Convert.ToInt16(numPort.Value);
+
+                try
+                {
+                    client = new TcpClient(host, port);
+                    lbSerwer.Invoke(new MethodInvoker(delegate { lbSerwer.Items.Add("Nawiązano połączenie z " + host + " Na porcie: " + port); }));
+                    NetworkStream ns = client.GetStream();
+                    reading = new BinaryReader(ns);
+                    writing = new BinaryWriter(ns);
+                    writing.Write("password");
+
+                    string passReceived;
+                    passReceived = reading.ReadString();
+                    if (passReceived == "NO")
+                    {
+                        lbSerwer.Invoke(new MethodInvoker(delegate { lbSerwer.Items.Add("" + passReceived + ""); }));
+                    }
+                    else
+                    {
+                        if (tBNick.TextLength == 0)
+                            tBNick.Invoke(new MethodInvoker(delegate { tBNick.Text = "Client"; }));
+                        tBNick.Invoke(new MethodInvoker(delegate { tBNick.Enabled = false; }));
+                        bStart.Invoke(new MethodInvoker(delegate { bStart.Enabled = false; }));
+                        bStop.Invoke(new MethodInvoker(delegate { bStop.Enabled = true; }));
+                        backgroundWorker2.RunWorkerAsync();
+                    }
+                    activeCall = true;
+                }
+                catch (Exception ex)
+                {
+                    lbSerwer.Invoke(new MethodInvoker(delegate { lbSerwer.Items.Add("" + ex.ToString() + ""); }));
+                    lbSerwer.Invoke(new MethodInvoker(delegate { lbSerwer.Items.Add("Błąd połączenia jako klient, uruchamiam serwer"); }));
+                    try
+                    {
+                        if (cBAddress.SelectedItem != null)
+                            adresIP = IPAddress.Parse(cBAddress.SelectedItem.ToString());
+                        else
+                            adresIP = IPAddress.Parse(cBAddress.Text.ToString());
+                        bStart.Enabled = false;
+                        bStop.Enabled = false;
+                    }
+                    catch{
+
+                        MessageBox.Show("Błędny format adresu IP" + adresIP, "Błąd");
+                        cBAddress.Invoke(new MethodInvoker(delegate { cBAddress.Text = ""; }));
+                        return;
+                    }
+
+
+                    try
+                    {
+                        server = new TcpListener(adresIP, port);
+                        server.Start();
+                        lbSerwer.Invoke(new MethodInvoker(delegate { lbSerwer.Items.Add("Oczekiwanie na połączenie"); }));
+                        client = server.AcceptTcpClient();
+                        NetworkStream ns = client.GetStream();
+                        reading = new BinaryReader(ns);
+                        writing = new BinaryWriter(ns);
+                        if (reading.ReadString() == "password")
+                        {
+                            writing.Write("OK");
+                            if (tBNick.TextLength == 0)
+                                tBNick.Invoke(new MethodInvoker(delegate { tBNick.Text = "Serwer"; }));
+                            activeCall = true;
+                            IPEndPoint IP = (IPEndPoint)client.Client.RemoteEndPoint;
+                          
+                            tBNick.Invoke(new MethodInvoker(delegate { tBNick.Enabled = false; }));
+                            lbSerwer.Invoke(new MethodInvoker(delegate { lbSerwer.Items.Add("[" + IP.ToString() + "] : Nawiązano połączenie"); }));
+                            bStart.Invoke(new MethodInvoker(delegate { bStart.Enabled = true; }));
+                            bStart.Invoke(new MethodInvoker(delegate { bStart.Text = "Stop"; }));
+                            bStop.Invoke(new MethodInvoker(delegate { bStop.Enabled = true; }));
+                            backgroundWorker2.RunWorkerAsync();
+                        }
+                        else
+                        {
+                            writing.Write("NO");
+                            activeCall = false;
+                            lbSerwer.Invoke(new MethodInvoker(delegate { lbSerwer.Items.Add("złe hasło"); }));
+                            backgroundWorker1.CancelAsync();
+                            //client.Close();
+                            //server.Stop();
+                            lbSerwer.Invoke(new MethodInvoker(delegate { lbSerwer.Items.Add("Zakończono pracę serwera ..."); }));
+                            bStart.Invoke(new MethodInvoker(delegate { bStart.Enabled = true; }));
+                            bStop.Invoke(new MethodInvoker(delegate { bStop.Enabled = false; }));
+                        }//client.Close();
+                         //server.Stop();
+                    }
+                    catch (Exception exr)
+                    {
+                        lbSerwer.Invoke(new MethodInvoker(delegate { lbSerwer.Items.Add("Błąd inicjacji serwera!"); }));
+                        bStart.Invoke(new MethodInvoker(delegate { bStart.Enabled = true; }));
+                        activeCall = false;
+                        MessageBox.Show(exr.ToString(), "Błąd");
+                    }
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (activeCall)
+            {
+                string messageReceived = reading.ReadString();
+                if (messageReceived != "END")
+                {
+                    wB1.Invoke(new MethodInvoker(delegate { wB1.DocumentText += "" + messageReceived + ""; }));
+                    IntPtr handle = Handle;
+                    FlashWindow(handle,false);
+                    Console.Beep();
+                }
+                else
+                {
+                    lbSerwer.Invoke(new MethodInvoker(delegate { lbSerwer.Items.Add("Wyłączono serwer!"); }));
+                    activeCall = false;
+                    bStart.Invoke(new MethodInvoker(delegate { bStart.Enabled = true; }));
+                    tBNick.Invoke(new MethodInvoker(delegate { tBNick.Enabled = true; }));
+                    bStop.Invoke(new MethodInvoker(delegate { bStop.Enabled = false; }));
+                }
+                
+            }
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+
+        private void tB1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Send();
+            }
+            else if (e.Alt && e.KeyCode == Keys.B)
+            {
+                Bold();
+            }else if(e.Alt && e.KeyCode == Keys.I)
+            {
+                Italic();
+            }
+
+        }
+
+        public void Send() {
+            if (activeCall)
+            {
+                string Nick = tBNick.Text;
+                string messageSend = tB1.Text;
+                if (messageSend.Length > 0)
+                {
+                    if (bold && italic)
+                        messageSend = "<b><em>" + messageSend + "</b></em>";
+                    else if (bold)
+                        messageSend = "<b>" + messageSend + "</b>";
+                    else if (italic)
+                        messageSend = "<em>" + messageSend + "</em>";
+
+                    wB1.Invoke(new MethodInvoker(delegate { wB1.DocumentText += "<div style = 'text-align: right;width:480px;' >" + Nick + " " + System.DateTime.Now.Hour + ":" + System.DateTime.Now.Minute + "<br>" + messageSend + "</div><hr>"; }));
+                    messageSend = "<div style='text-align: left;width:480px;'>"+Nick+" "  + System.DateTime.Now.Hour + ":" + System.DateTime.Now.Minute + "<br>" + messageSend + "</div><hr>";
+                    tB1.Invoke(new MethodInvoker(delegate { tB1.Text = ""; }));
+                    writing.Write(messageSend);
+                }
+            }
+        }
+
+
+        public void Bold()
+        {
+            if (bold)
+            {
+                bold = false;
+                btBold.BackColor = SystemColors.Control;
+            }
+            else
+            {
+                bold = true;
+                btBold.BackColor = SystemColors.Info;
+            }
+        }
+
+        public void Italic()
+        {
+            if (italic)
+            {
+                italic = false;
+                btItalic.BackColor = SystemColors.Control;
+            }
+            else
+            {
+                italic = true;
+                btItalic.BackColor = SystemColors.Info;
+            }
+        }
+
+        private void btItalic_Click(object sender, EventArgs e)
+        {
+            Italic();
+        }
+
+        private void btBold_Click(object sender, EventArgs e)
+        {
+            Bold();
+            
+        }
+
+
+    }
+}
